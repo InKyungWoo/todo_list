88db1e1c792d14eee82a77cd6089496060a9f3bd
Inkyung <chelsea1201@naver.com>
2023-05-29 00:08:31 +0900

    template 수정

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Todo
 from .forms import TodoForm
+from datetime import datetime
 # Create your views here.
 
 def todo_list(request):
     todos = Todo.objects.filter(complete=False)
-    return render(request, 'todo/todo_list.html',{'todos': todos})
+    now = datetime.now()
+    return render(request, 'todo/todo_list.html',{'todos': todos, 'now':now})
 
 def todo_detail(request, pk):
     todo = Todo.objects.get(id=pk)
